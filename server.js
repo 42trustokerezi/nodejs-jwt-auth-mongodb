32c40d9392122fe667460c7f8e3dcc81eaf5c376
Trust Okerezi <trustokerezi42@gmail.com>
2022-10-21 20:40:20 +0100

    routes and headers

--- server.js
+++ server.js
@@ -69,6 +69,10 @@ app.get('/', (req, res) => {
     res.json({ message: 'Welcome to intel-corp'});
 });
 
+// routes
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
+
 //set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, ()=>{
